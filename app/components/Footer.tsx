950b1c9af847c701fa8f2b4cd774cf71e685f31d
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-07 10:52:05 +0800

    Some edited

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className='padding-container max-container flex w-full flex-col gap-14'>
         <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
           <Link href='/' className='mb-10'>
-            <Image src='hilink-logo.svg' alt='logo' width={74} height={24} />
+            <Image src='hitours-logo.svg' alt='logo' width={74} height={24} />
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
@@ -51,7 +51,7 @@ const Footer = () => {
         </div>
 
         <div className='border bg-gray-20' />
-        <p className='regular-14 w-full text-center text-gray-30'>Hilink | All rights reserved</p>
+        <p className='regular-14 w-full text-center text-gray-30'>HiTours | All rights reserved</p>
       </div>
     </footer>
   );
